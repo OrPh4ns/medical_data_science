3bbb92cfb26100b115b25e0744076c81d7925541
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2023-05-26 14:46:26 +0200

    moved resources to Resources folder #34

--- project/project/settings.py
+++ project/project/settings.py
@@ -41,7 +41,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'Resources/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -100,7 +100,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "Resources/static"),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
