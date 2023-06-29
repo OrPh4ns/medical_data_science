1b7a661c6d2a8d88c72a798563b8429373af3be1
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2023-06-30 00:31:47 +0200

    added functions router #42

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.urls import path
-# from Controllers import ImageController, PatientController, StudyController, SeriesController, HomeController
-from Controllers import HomeController
-# image = ImageController.ImageController()
+# from Controllers import , PatientController, StudyController, SeriesController, HomeController
+from Controllers import HomeController, UserController,ImageController
+
+image = ImageController.ImageController()
 # patient = PatientController.PatientController()
 # study = StudyController.StudyController()
 # series = SeriesController.SeriesController()
 home = HomeController.HomeController()
+user = UserController.UserController()
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home.index)
+    path('', home.index),
+    path('login', user.login),
+    path('logout', user.logout),
+    path('images', image.show_images),
+    path('images/<int:page_id>/', image.show_images),
+    path('image/<int:image_id>/', image.show_image)
 ]
