3d916d3019526785f6637be0051844a7f5aac0d5
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2023-07-05 12:01:10 +0200

    add serach routing #53

--- project/project/urls.py
+++ project/project/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('logout', user.logout),
     path('images', image.show_images),
     path('images/<int:page_id>/', image.show_images),
-    path('image/<int:image_id>/', image.show_image)
+    path('image/<int:image_id>/', image.show_image),
+    path('search', image.search_images)
 ]
